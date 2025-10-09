f02f9f7399f743fe3175fa8b264ef843d8319fdb
D4viMartinsLima <martinslimafkc@gmail.com>
2025-10-09 17:46:16 -0300

    UI setup done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter.tix import ButtonBox
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -24,7 +26,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image= tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 35), fg=GREEN, bg=YELLOW, highlightthickness=0)
+timer_label.grid(column=1,row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✓", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW, highlightthickness=0)
+check_mark.grid(column=1, row=3)
 
 
 window.mainloop()
